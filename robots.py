c7f95dac70ab535fa9c47a01d78ad164e209a3e7
Martin Morrison <martin@syndeo.co.uk>
2014-02-12 11:41:24 +0100

    Flesh out the structure

--- robots.py
+++ robots.py
@@ -1,1 +1,13 @@
-print("I'm alive!")
+#!/usr/bin/python
+
+class Robot:
+    def __init__(self, name):
+        self.name = name
+
+    def run(self):
+        print("I'm alive!")
+
+
+if __name__ == "__main__":
+    robot = Robot("Hal")
+    robot.run()
